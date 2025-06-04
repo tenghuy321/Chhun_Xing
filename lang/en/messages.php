7c9a9230d3713df1039db55b7549d8403b5ab0f9
Tenghuy <tenghuyly35@gmailcom>
2025-06-04 11:29:17 +0700

    first commit

---
+++ lang/en/messages.php
@@ -0,0 +1,17 @@
+<?php
+    return [
+        'Home' => 'Home',
+        'About Us' => 'About Us',
+        'Real Estate Project' => 'Real Estate Project',
+        'Our Services' => 'Our Services',
+        'Events' => 'Events',
+        'Contact' => 'Contact Us',
+
+        'CHHUN XING' => 'CHHUN XING',
+        'CITY' => 'CITY',
+        'banner_text' => 'Building a Brighter Future with Chhun Xing City Group',
+
+        'read_more' => 'Read More',
+        'back' => 'Back'
+
+    ];
---
+++ lang/kh/messages.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+        'Home' => 'ទំព័រដើម',
+        'About Us' => 'អំពីយើងខ្ញុំ',
+        'Real Estate Project' => 'គម្រោងអចលនទ្រព្យ',
+        'Our Services' => 'សេវាកម្មយើងខ្ញុំ',
+        'Events' => 'Events',
+        'Contact' => 'ការទំនាក់ទំនង',
+
+        'CHHUN XING' => 'ឈុនស៊ីង',
+        'CITY' => 'គ្រុប',
+        'banner_text' => 'Building a Brighter Future with Chhun Xing City Group',
+
+        'read_more' => 'ព័ត៌មានបន្ថែម',
+        'back' => 'ត្រឡប់ក្រោយ'
+    ];
